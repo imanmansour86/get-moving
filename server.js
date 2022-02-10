0c01b88450fcf064dab229340e7c2c14f860c040
imanmansour86 <imanmansour86@gmail.com>
2022-02-10 12:24:36 -0800

    installing session sequelize

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const path = require("path");
 const express = require("express");
 const session = require("express-session");
-const routes = require("./routes");
+const routes = require("./controllers");
 const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
 
