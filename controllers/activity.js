e398e382f34f84f7e61711ce55ea6ab7c65298e7
jsncorn <jsncourn@gmail.com>
2022-02-10 10:49:12 -0800

    added routers to activity.js, fixed semicolon issues in users.js

--- controllers/activity.js
+++ controllers/activity.js
@@ -1,11 +1,34 @@
 const router = require('express').Router();
 
+//get all activities
 router.get('/', async (req, res) => {
-    try{
+    try {
 
-    } catch {
-        
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err)
     }
 })
 
-module.exports = router
+//add activity 
+router.post('/', async (req, res) => {
+    try {
+
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err)
+    }
+})
+
+//view one activity
+router.get('/:id', async (req, res) => {
+    try {
+        const input = req.params.id;
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err)
+    }
+})
+
+
+module.exports = router;
