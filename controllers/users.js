640fb654f939c6f71309c249ea7bb5899b7b3840
jsncorn <jsncourn@gmail.com>
2022-02-10 11:38:14 -0800

    added account.js with base routes, added catches to users

--- controllers/users.js
+++ controllers/users.js
@@ -2,16 +2,19 @@ const router = require('express').Router();
 
 router.get('/:id', async(req, res) => {
     try{
-
+        console.log('display single user')
     } catch (err) {
+        res.status(500).json(err);
+        console.log(err)
     }
 });
 
 router.get('', async(req, res) => {
     try {
-
+        console.log('display current user page')
     } catch (err) {
-
+        res.status(500).json(err);
+        console.log(err)
     }
 });
 
