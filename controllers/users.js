abe7546818a47aaedb5e736ff1ebc30003c9661d
jsncorn <jsncourn@gmail.com>
2022-02-10 11:33:15 -0800

    added base routes for users

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,18 @@
 const router = require('express').Router();
 
+router.get('/:id', async(req, res) => {
+    try{
 
+    } catch (err) {
+    }
+});
+
+router.get('', async(req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+});
 
 module.exports = router;
