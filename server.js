1c7daf6aae564ae776f1fd40bfe47c78fb19e676
imanmansour86 <imanmansour86@gmail.com>
2022-02-10 11:30:19 -0800

    adding required packages in server.js and add session and cookies

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
 //Requirements
-const exphbs = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const routes = require("./routes");
+const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
 
-//Set up Handlebars 
-const hbs = exphbs.create();
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+//Set up Handlebars
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Set up Handlebars for express
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
