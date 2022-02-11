93a242604cad2828a61e911cb4a5d5ed6c3b47c3
jsncorn <jsncourn@gmail.com>
2022-02-11 10:22:22 -0800

    added delete route and get by id route

--- controllers/api/activityRoutes.js
+++ controllers/api/activityRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { Activity } = require('../../models')
+const { Activity, User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 //get all activities
 router.get('/', async (req, res) => {
@@ -28,13 +29,46 @@ router.post('/', async (req, res) => {
 //view one activity
 router.get('/:id', async (req, res) => {
     try {
-        const input = req.params.id;
-        console.log('display specific activity' + input);
+      const activityData = await Activity.findByPk(req.params.id, {
+        include: [
+          {
+            model: User,
+            attributes: ['name'],
+          },
+        ],
+      });
+  
+      const activity = activityData.get({ plain: true });
+  
+      res.render('activity', {
+        ...activity,
+        logged_in: req.session.logged_in
+      });
     } catch (err) {
-        res.status(500).json(err);
-        console.log(err)
+      res.status(500).json(err);
     }
-});
+  });
+
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const activityData = await Activity.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!activityData) {
+        res.status(404).json({ message: 'No activity found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(activityData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
 
 module.exports = router;
