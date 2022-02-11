7a2d9dfcf7e6d618fba053643a5d6545cf2c186a
jsncorn <jsncourn@gmail.com>
2022-02-11 10:28:35 -0800

    added dependencies to index

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,7 @@
 const router = require("express").Router();
+const userRoutes = require('./userRoutes');
+const activityRoutes = require('./activityRoutes');
 
+router.use('/users', userRoutes);
+router.use('/projects', activityRoutes)
 module.exports = router;
